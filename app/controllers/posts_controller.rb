e0f4cf8a1bad8c411132156db358117fbce1c90c
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-10-31 18:56:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,8 @@ helper_method :params
   def index
     @authors = Author.all
      if !params[:author].blank?
-    @posts = Post.where(author: params[:author])
-  elsif !params[:date].blank?
+  @posts = Post.by_author(params[:author])
+elsif !params[:date].blank?
     if params[:date] == "Today"
       @posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
     else
